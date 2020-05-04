eec4dcc6194e3b12208af208efbfba03869524c9
VSUPMM <valyafenris@gmail.com>
2020-05-04 21:17:16 +0300

    com

--- src/main/java/com/example/demo/excpection/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.excpection;
-
-public class TaskNotFoundException extends RuntimeException{
-    public TaskNotFoundException(Integer id) {
-        super("Could not find task " + id);
-    }
-}
